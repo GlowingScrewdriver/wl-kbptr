2328ce112b24d70349ed37b9616211d5e0209603
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2026-03-24 20:47:24 +0000

    Generate debian/ using debmake

---
+++ debian/bug-control.ex
@@ -0,0 +1,3 @@
+# Installed as usr/share/bug/wl-kbptr/control in wl-kbptr
+# See dh_bugfiles(1) and /usr/share/doc/reportbug/README.developers.gz
+# It is the bug control file containing some directions for the bug reporting tool.
---
+++ debian/bug-script.ex
@@ -0,0 +1,3 @@
+# Installed as usr/share/bug/wl-kbptr or usr/share/bug/wl-kbptr/script in wl-kbptr
+# See dh_bugfiles(1) and /usr/share/doc/reportbug/README.developers.gz
+# This is the script to be run by the bug reporting program for generating a bug report template.
